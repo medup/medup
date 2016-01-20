540f34231139a7442ca8f7d69b18f453709e1537
Brian Tedder <imbrianted@gmail.com>
2016-01-20 15:43:36 -0700

    getting started with hapi

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 'use strict';
-const Hapi = require('hapi');
-const server = new Hapi.Server();
 
-server.connection({
-  port: process.env.PORT || 3000,
-});
+const Hapi = require('hapi'),
+      Glue = require('glue'),
+      manifest = require('./config/manifest.json');
 
-server.start(() => {
-  console.log('server running at', server.info.port);
+if (process.env.NODE_ENV !== 'production') {
+  manifest.registrations.push({
+    "plugin": {
+      "register": "blipp"
+    }
+  });
+
+  let good = manifest.registrations.find(p => p.plugin.register === 'good');
+  if (good) {
+    good.plugin.options.reporters[0].events['request'] = "*";
+    good.plugin.options.reporters[0].events['log'] = null;
+  }
+}
+
+Glue.compose(manifest, { relativeTo: __dirname }, (err, server) => {
+  if (err) console.error('server.register err:', err);
+
+  server.start(() => {
+    console.log("Server is listening on", server.info.port);
+  });
 });
