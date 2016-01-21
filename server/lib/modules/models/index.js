a41d2e5f07fad346f48e9d79f8668a9b085ba02b
Brian Tedder <imbrianted@gmail.com>
2016-01-21 14:10:05 -0700

    moved models into module

---
+++ server/lib/modules/models/index.js
@@ -0,0 +1,30 @@
+'use strict';
+const Memory = require('sails-memory');
+
+exports.register = (plugin, options, next) => {
+
+  plugin.register({
+    register: require('dogwater'),
+    options: {
+      adapters: {
+        memory: Memory
+      },
+      connections: {
+        local: {
+          adapter: 'memory'
+        }
+      },
+      models: [
+        require('./user.model')
+      ]
+    }
+  }).then(err => {
+    if (err) console.error(err);
+  });
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'models'
+};
--- server/lib/modules/user/index.js
+++ server/lib/modules/user/index.js
@@ -3,25 +3,6 @@ const Memory = require('sails-memory');
 
 exports.register = (plugin, options, next) => {
 
-  plugin.register({
-    register: require('dogwater'),
-    options: {
-      adapters: {
-        memory: Memory
-      },
-      connections: {
-        local: {
-          adapter: 'memory'
-        }
-      },
-      models: [
-        require('./user.model')
-      ]
-    }
-  }).then(err => {
-    if (err) console.error(err);
-  });
-
   plugin.route({
     path: '/user/signin',
     method: 'POST',
