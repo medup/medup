6a1fde53915ae9908ba6ea797f36ac8ad6ba19a3
dmeidlin <git_hub@nym.hush.com>
2016-01-22 13:14:15 -0500

    Update gulpfile before rebase.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,34 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
-var mocha = require('gulp-mocha');
-var casper = require('gulp-casperjs-options');
-var karma = require('gulp-karma2');
-var express = require('gulp-express');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var beautify = require('gulp-beautify');
-var cssbeautify = require('gulp-cssbeautify');
-var copy = require('gulp-copy');
-var strip = require('gulp-strip-debug');
-var del = require('del');
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  bower = require('bower'),
+  concat = require('gulp-concat'),
+  sass = require('gulp-sass'),
+  minifyCss = require('gulp-minify-css'),
+  rename = require('gulp-rename'),
+  sh = require('shelljs'),
+  mocha = require('gulp-mocha'),
+  casper = require('gulp-casperjs-options'),
+  karma = require('gulp-karma2'),
+  express = require('gulp-express'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  beautify = require('gulp-beautify'),
+  cssbeautify = require('gulp-cssbeautify'),
+  copy = require('gulp-copy'),
+  strip = require('gulp-strip-debug'),
+  del = require('del'),
+  protractor = require('gulp-angular-protractor'),
+  nodemon = require('gulp-nodemon'),
+  notify = require('gulp-notify'),
+  livereload = require('gulp-livereload'),
+  browserify = require('gulp-browserify'),
+  embedlr = require('gulp-embedlr'),
+  lrserver = require('tiny-lr')();
 
+var filepath = {
+}
   // in what order should the files be concatenated
-var clientIncludeOrder = require('./include.conf.js');
+var clientIncludeOrder = [];
 
 // gulp setup
 var config = require('package.json');
@@ -37,19 +46,84 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
+//Uglify the files
 gulp.task('uglify', function() {
     return gulp.src('js/*.js')
       .pipe(uglify())
       .pipe(gulp.dest(dist/assets));
 });
 
-// concat all the js files
+/**/
+gulp.task('scripts', function() {
+    return gulp.src(clientIncludeOrder)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
 
+//Refresh when files change
+gulp.task('refresh', function() {
+	// listen for changes
+	livereload.listen();
+	// configure nodemon
+	nodemon({
+		// the script to run the app
+		script: 'app.js',
+		ext: 'js'
+	}).on('restart', function(){
+		// when the app has restarted, run livereload.
+		gulp.src('app.js')
+			.pipe(livereload())
+			.pipe(notify('Reloading page, please wait...'));
+	})
+})
 
+// Browserify task
+gulp.task('browserify', function() {
+/* Single point of entry (make sure not to src ALL your files,
+browserify will figure it out for you)*/
+  gulp.src(['app/scripts/main.js'])
+  .pipe(browserify({
+    insertGlobals: true,
+    debug: true
+  }))
+
+gulp.task('watch', ['lint'], function() {
+
+// Watch our scripts
+  gulp.watch(['app/scripts/*.js', 'app/scripts/**/*.js'],[
+    'lint',
+    'browserify'
+  ]);
+});
+
+// Set up an express server (but not starting it yet)
+var server = express();
+// Add live reload
+server.use(livereload({port: livereloadport}));
+// Use our 'dist' folder as rootfolder
+server.use(express.static('./dist'));
+// Because I like HTML5 pushstate .. this redirects everything back to our index.html
+server.all('/*', function(req, res) {
+    res.sendfile('index.html', { root: 'dist' });
+});
+
+// Dev task
+gulp.task('dev', function() {
+  // Start webserver
+  server.listen(serverport);
+  // Start live reload
+  lrserver.listen(livereloadport);
+  // Run the watch task, to keep taps on changes
+  gulp.run('watch');
+});
+  
 var paths = {
   sass: ['./scss/**/*.scss']
 };
-
+  
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
