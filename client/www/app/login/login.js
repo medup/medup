937b16e9091b83e92294912f5f674524c98f76b5
kayladowling <kaydowling2@gmail.com>
2016-01-22 21:57:03 -0600

    (feat)

--- client/www/app/login/login.js
+++ client/www/app/login/login.js
@@ -1,11 +1,21 @@
 (function() {
   'use strict';
-// QUESTION - on line 4. 
+// app name : starter
+//LoginService dependency is defined in the services.js file
+// $ionicPopup dependency for a simple popup
+// $state for the transition to the next view
   angular.module('starter.login', [])
-    .controller('LoginCtrl', function($scope, $state) {
+    .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
      $scope.data = {};
      $scope.login = function() {
-      console.log("login user: " + $scope.data.username);
+      LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+       $state.go('tab.dash'); 
+      }).error(function(data) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Login Failed',
+          template: 'Please Check your credentials!'
+        });
+      })
      };
     });
 })();
--- client/www/app/services.js
+++ client/www/app/services.js
@@ -9,9 +9,33 @@
 
   angular
     .module('starter.services', [])
-    .factory('Name', function() {
+    .service('LoginService', function($q) {
+      return {
+        //expects the name and password from the user
+        // the promises to verify the user to a REST server - async
+        loginUser: function() {
+          var deferred = $q.defer();
+          var promise = deferred.promise;
+          //if access is granted / promise is resolved
+          if (name == 'user' && pw == 'secret') {
+            deferred.resolve('Welcome ' + name + '!');
+          } else {
+            deferred.reject('Wrong credentials.');
+          }
+          promise.success = function(fn) {
+            promise.then(fn);
+            return promise;
+          }
+          promise.error = function() {
+            promise.then(null, fn);
+            return promise;
+          }
+          return promise;
+        }
+      }
+    })
 
-    });
+
 
 
 })();
