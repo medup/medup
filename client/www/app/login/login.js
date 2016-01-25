656d82da8eb368737ce611dec9f3d8fe4592f2a2
kayladowling <kaydowling2@gmail.com>
2016-01-24 20:35:18 -0600

    (feat) register & login -  adding controller for register & state.go changed to the login controller

--- client/www/app/login/login.js
+++ client/www/app/login/login.js
@@ -9,7 +9,7 @@
      $scope.data = {};
      $scope.login = function() {
       LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-       $state.go('tab.dash'); 
+       $state.go('state.dashboard'); 
       }).error(function(data) {
         var alertPopup = $ionicPopup.alert({
           title: 'Login Failed',
