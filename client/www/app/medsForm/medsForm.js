d08298cbceda87ee1f5bf5c6b80d6f5047b5e151
dmeidlin <git_hub@nym.hush.com>
2016-01-27 18:50:30 -0500

    (feat) add MedsForm controller

---
+++ client/www/app/medsForm/medsForm.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('starter.dashboard', ['ionic', 'ionic-material'])
+    .controller('MedsFormCtrl', MedsFormCtrl);
+  MedsFormCtrl.$inject = ['$scope', '$state', '$ionicModal', '$timeout', 'MedService'];
+
+  function MedsFormCtrl($scope, $state, $stateParams, MedService) {
+    
+  }
+})();
