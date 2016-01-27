bd7915fbc2caada2d91827fed02dfd919c140830
dmeidlin <git_hub@nym.hush.com>
2016-01-27 15:06:00 -0500

    (fix) update before rebase

--- client/www/app/services.js
+++ client/www/app/services.js
@@ -11,7 +11,7 @@
     .module('starter.services', [])
     .service('AuthService', AuthService)
     .service('MedService', MedService)
-    .factory('Medications', Medications);
+    .factory('Medications', MedService);
    
   AuthService.$inject = ['$window', '$state', '$http'];
   MedService.$inject = ['$state', '$http'];
