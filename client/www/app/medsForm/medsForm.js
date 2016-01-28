17f9cc11cecf6540b7d4489a9453ee1c73002028
dmeidlin <git_hub@nym.hush.com>
2016-01-28 00:41:54 -0500

    (refactor) store form content using ng-model directives

--- client/www/app/medsForm/medsForm.js
+++ client/www/app/medsForm/medsForm.js
@@ -9,11 +9,21 @@
   function MedsFormCtrl($scope, $state, $stateParams, MedService, Medications) {
     $scope.medId = $stateParams.medName;
     $scope.userMeds = Medications.userMeds;
-    $scope.userMeds.localMeds.forEach(function (medication) {
-      if (medication.id === parseInt($scope.medId)) $scope.med = medication;
-    });
-    $scope.goHome = function() {
-      $state.go('dashboard');
+    $scope.form = {
+      name: '',
+      dosage: '',
+      timeNeeded: '',
+      instruction: ''
     };
+    $scope.userMeds.localMeds.forEach(function (medication) {
+      if (medication.id === parseInt($scope.medId)) {
+	$scope.form.name = medication.name;
+	$scope.form.dosage = medication.dosage;
+	$scope.form.timeNeeded = medication.reminder;
+	$scope.form.instruction = medication.instruction;
+      }
+    });
+
+
   };
 })();
