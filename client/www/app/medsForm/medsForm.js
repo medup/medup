2258d19a1581824b7a542a8316b39956b8e2aa0d
dmeidlin <git_hub@nym.hush.com>
2016-01-28 02:57:28 -0500

    (feat) add route to medsForm when 'add' icon clicked

--- client/www/app/medsForm/medsForm.js
+++ client/www/app/medsForm/medsForm.js
@@ -9,21 +9,8 @@
   function MedsFormCtrl($scope, $state, $stateParams, MedService, Medications) {
     $scope.medId = $stateParams.medName;
     $scope.userMeds = Medications.userMeds;
-    $scope.form = {
-      name: '',
-      dosage: '',
-      timeNeeded: '',
-      instruction: ''
-    };
     $scope.userMeds.localMeds.forEach(function (medication) {
-      if (medication.id === parseInt($scope.medId)) {
-	$scope.form.name = medication.name;
-	$scope.form.dosage = medication.dosage;
-	$scope.form.timeNeeded = medication.reminder;
-	$scope.form.instruction = medication.instruction;
-      }
+      if (medication.id === parseInt($scope.medId)) $scope.med = medication;
     });
-
-
   };
 })();
