854308a367b8389755e3370d545d4d58ffb1afb0
Robert Lin <Rlin415@gmail.com>
2016-01-30 15:57:57 -0800

    configured app module

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -1,1 +1,61 @@
 "use strict";
+
+angular.module('app', [])
+  .config(($stateProvider, $urlRouterProvider, $httpProvider, $urlRouterProvider) => {
+    $urlRouterProvider.otherwise('/dashbaord');
+    $stateProvider
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashbaord/dashbaord.html',
+        controller: 'DashboardCtrl'
+      })
+      .state('signin', {
+        url: '/signin',
+        templateUrl: 'app/auth/signin.html',
+        controller: 'AuthCtrl'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'app/auth/signup.html',
+        controller: 'AuthCtrl'
+      })
+      .state('medications', {
+        url: '/medications',
+        templateUrl: 'app/auth/medications.html',
+        controller: 'MedicationCtrl'
+      });
+      $httpProvider.interceptors.push('AttachTokens');
+    })
+    .factory('AttachTokens', ($window) => {
+
+      let attach = {
+        request: (object) => {
+
+          let jwt = $window.localStorage.getItem('com.pillMeNow');
+
+          if (jwt) {
+            object.headers['x-access-token'] = jwt;
+          }
+
+          object.headers['Allow-Control-Allow-Origin'] = '*';
+
+          return object;
+
+        }
+      };
+
+      return attach;
+
+    })
+    .run(($rootScope, $state, Auth) => {
+      $rootScope.on('$stateChangeStart', (evt, toState) => {
+
+        if (toState.name === 'signin') return;
+
+        if (!Auth.isAuth() && toState.name !== 'signup') {
+          evt.preventDefault();
+          $state.go('signin');
+        }
+
+      });
+    });
