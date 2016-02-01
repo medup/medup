5c2ab5d6f3719b0f2d7ae52267f662efa4455aa1
Brian Tedder <imbrianted@gmail.com>
2016-02-01 16:47:45 -0700

    (chore) remove console.log

--- server/config/index.js
+++ server/config/index.js
@@ -40,8 +40,6 @@ module.exports = manifest => {
     includePaths: neat.includePaths
   };
 
-  console.log(sassOptions);
-
   let hapiSass = manifest.registrations.find(p => p.plugin.register === 'hapi-sass');
   if (hapiSass) {
     hapiSass.plugin.options = sassOptions;
