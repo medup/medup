a4320b7d67cce38d36d0ea88579e1f1b14262cdb
Brian Tedder <imbrianted@gmail.com>
2016-02-01 17:17:02 -0700

    (fix) handle web client files

--- server/routes.js
+++ server/routes.js
@@ -43,8 +43,7 @@ exports.register = (plugin, options, next) => {
     { method: 'GET', path: '/{param*}', config: { auth: false,
                                                     handler: {
                                                       directory: {
-                                                        path: 'assets' } } } },
-
+                                                        path: 'public' } } } },
     { method: 'POST', path: '/user/signup', config: { auth: false, handler: handlers['Users'].signup, validate: internals.routeValidation.signup } },
     { method: 'POST', path: '/user/signin', config: { auth: false, handler: handlers['Users'].signin, validate: internals.routeValidation.signin } },
     { method: 'GET', path: '/restricted', config: { auth: 'jwt', handler: handlers['Restricted'] } },
