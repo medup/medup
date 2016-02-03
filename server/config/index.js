19dca17fbec1e71b7325e365a80670593df56b48
Brian Tedder <imbrianted@gmail.com>
2016-02-03 12:48:30 -0700

    (chore) refactor server config

--- server/config/index.js
+++ server/config/index.js
@@ -4,45 +4,61 @@ const crypto = require('crypto'),
       neat = require('node-neat'),
       path = require('path');
 
+const internals = {};
+
 module.exports = manifest => {
-  /* TODO refactor this */
+
+  /* Set port or use assigned */
+  manifest.connections[0].port = internals.port;
+
+  /* Static files relative paths */
+  manifest.connections[0].routes.files = {
+    relativeTo: __dirname + '/../'
+  };
+
+  /* Set random token secret */
+  process.env.tokenSecret = crypto.randomBytes(16)
+                                    .toString('base64');
+
+  /* Configure Node-sass */
+  let hapiSass = manifest.registrations.find(p => p.plugin.register === 'hapi-sass');
+  if (hapiSass) {
+    hapiSass.plugin.options = internals.sassOptions;
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    internals.localConfig(manifest);
+  }
+};
+
+internals.port = process.env.PORT || 3000;
+
+/* Node-sass config */
+internals.sassOptions = {
+  src: path.join(__dirname, '/../public/assets/sass'),
+  dest: path.join(__dirname, '/../public/assets/css'),
+  force: process.env.NODE_ENV === 'production' ? false : true,
+  includePaths: neat.includePaths
+};
+
+/* Local development config */
+internals.localConfig = (manifest) => {
+  process.env.keySecret = crypto.randomBytes(16)
+                                  .toString('base64');
+
+  manifest.registrations.push({
+    "plugin": {
+      "register": "blipp"
+    }
+  });
+
   if (!process.env.NODE_ENV) {
     manifest.connections[0].host = 'localhost';
   }
 
-  if (process.env.NODE_ENV !== 'production') {
-    process.env.tokenSecret = crypto.randomBytes(16)
-                                    .toString('base64');
-    process.env.keySecret = crypto.randomBytes(16)
-                                    .toString('base64');
-
-    manifest.registrations.push({
-      "plugin": {
-        "register": "blipp"
-      }
-    });
-
-    let good = manifest.registrations.find(p => p.plugin.register === 'good');
-    if (good) {
-      good.plugin.options.reporters[0].events['request'] = "*";
-      good.plugin.options.reporters[0].events['log'] = null;
-    }
-  }
-
-  manifest.connections[0].routes.files = {
-    relativeTo: __dirname + '/../'
-  };
-  manifest.connections[0].port = process.env.PORT || 3000;
-
-  let sassOptions = {
-    src: path.join(__dirname, '/../public/assets/sass'),
-    dest: path.join(__dirname, '/../public/assets/css'),
-    force: true,
-    includePaths: neat.includePaths
-  };
-
-  let hapiSass = manifest.registrations.find(p => p.plugin.register === 'hapi-sass');
-  if (hapiSass) {
-    hapiSass.plugin.options = sassOptions;
+  let good = manifest.registrations.find(p => p.plugin.register === 'good');
+  if (good) {
+    good.plugin.options.reporters[0].events['request'] = "*";
+    good.plugin.options.reporters[0].events['log'] = null;
   }
 };
