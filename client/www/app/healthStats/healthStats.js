6cb16626e9deaf28618d3c473a7932314f444063
Jefri Vanegas <vjefri@gmail.com>
2016-02-03 04:19:07 -0800

    (feat) created files to begin healthStats view

---
+++ client/www/app/healthStats/healthStats.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('starter.main', ['ionic', 'ionic-material', 'nvd3'])
+    .controller('HealthStatsCtrl', HealthStatsCtrl);
+  HealthStatsCtrl.$inject = ['$scope', '$state'];
+
+  function HealthStatsCtrl($scope, $state) {
+
+  }
+})();
