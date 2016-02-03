aeef28cd5da4c43b4ff93bf2255adf4866fe48f7
kayladowling <kaydowling2@gmail.com>
2016-02-02 21:57:46 -0600

    (feat) dashboard- auth view combine

--- server/public/app/components/components.js
+++ server/public/app/components/components.js
@@ -2,9 +2,11 @@
 
 import angular from 'angular';
 import Dashboard from './dashboard/dashboard';
+import Auth from './auth/auth';
 
 let componentModule = angular.module('medup-web.components', [
-  Dashboard.name
+  Dashboard.name,
+  Auth.name
 ]);
 
 export default componentModule;
--- server/public/app/components/dashboard/dashboard.component.js
+++ server/public/app/components/dashboard/dashboard.component.js
@@ -1,14 +1,22 @@
-"use strict";
+'use strict';
 
 import template from './dashboard.html';
 import controller from './dashboard.controller';
+import signinController from '../auth/dashboard.signin.controller';
+import signupController from '../auth/dashboard.signup.controller';
 
 let dashboardComponent = {
   restrict: 'E',
   bindings: {},
   template,
   controller,
+  signupController,
+  signinController,
   controllerAs: 'vm'
 };
 
 export default dashboardComponent;
+
+
+
+
