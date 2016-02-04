0fb522df09131302fc6693b364a193fead6a09c1
kayladowling <kaydowling2@gmail.com>
2016-02-04 12:59:48 -0600

    (feat) files renamed

---
+++ server/public/app/components/medications/medications.component.js
@@ -0,0 +1,15 @@
+'use strict';
+//to do update
+import template from './medications.html';
+import medicationsController from './medications.controller';
+
+
+let medicationsComponent = {
+  restrict: 'E',
+  bindings: {},
+  template,
+  controller,
+  controllerAs: 'vm'
+};
+
+export default medicationsComponent;
