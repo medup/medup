6a218b3096a84c637584965a767f805fbacd5702
Robert Lin <Rlin415@gmail.com>
2016-02-04 11:27:35 -0800

    (feat) add-medication controller

--- server/public/app/components/add-medication/add-medication.controller.js
+++ server/public/app/components/add-medication/add-medication.controller.js
@@ -1,8 +1,30 @@
 "use strict";
 
 class addMedicationController {
-  constructor() {
+  constructor($scope, addMedicationFactory) {
     this.name = 'addMedication';
+    this.scope = $scope;
+    this.scope.addMedicationFactory = addMedicationFactory;
+    this.scope.medication = {
+      notifications: []
+    };
+    this.scope.times = [
+      '8:30am',
+      '12:30pm',
+      '6:30pm',
+      '9:30pm'
+    ];
+    this.scope.submitMedication = this.submitMedication;
+  }
+  submitMedication(medication) {
+    console.log('medication', medication);
+    this.addMedicationFactory.addMedication(medication)
+      .then((res) => {
+        console.log('response', res);
+      })
+      .catch((err) => {
+        console.error('error', err);
+      });
   }
 }
 
