65cd5ba9b7026a0f5395f5aae9117386f267c62a
Brian Tedder <imbrianted@gmail.com>
2016-02-09 16:17:04 -0700

    (chore) cleanup and default route

--- server/public/app/app.js
+++ server/public/app/app.js
@@ -13,70 +13,36 @@
     Common.name,
     Components.name
   ])
-    .config(($httpProvider, $urlRouterProvider) => {
-      //$urlRouterProvider.otherwise('/');
-      $httpProvider.interceptors.push('AttachTokens');
-    })
-    .component('medupWeb', AppComponent)
-    .factory('AttachTokens', function($window) {
-      var attach = {
-        request: function(object) {
-          var jwt = $window.localStorage.getItem('com.medUp');
-          if (jwt) {
-            object.headers['Authorization'] = jwt;
-          }
-          return object;
+  .config(($httpProvider, $urlRouterProvider) => {
+    $urlRouterProvider.otherwise('/');
+    $httpProvider.interceptors.push('AttachTokens');
+  })
+  .component('medupWeb', AppComponent)
+  .factory('AttachTokens', function($window) {
+    var attach = {
+      request: function(object) {
+        var jwt = $window.localStorage.getItem('com.medUp');
+        if (jwt) {
+          object.headers['Authorization'] = jwt;
         }
-      };
-      return attach;
-    })
-    .run(function($rootScope, $state, AuthFactory) {
-      $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
+        return object;
+      }
+    };
+    return attach;
+  })
+  .run(function($rootScope, $state, AuthFactory) {
+    $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
 
-        if (toState.name === 'dashboard.signin') {
-          return;
-        }
+      if (toState.name === 'dashboard.signin') {
+        return;
+      }
 
-        if (!AuthFactory.hasToken() && toState.name !== 'dashboard.signin' && toState.name !== 'dashboard.signup' && toState.name !== 'dashboard.splash') {
-          e.preventDefault();
-          $state.go('dashboard.signin');
-        }
-      });
+      if (!AuthFactory.hasToken()
+          && toState.name !== 'dashboard.signin'
+          && toState.name !== 'dashboard.signup'
+          && toState.name !== 'dashboard.splash') {
+        e.preventDefault();
+        $state.go('dashboard.signin');
+      }
     });
-    // .service('AuthServices', AuthServices);
-    // .factory('AttachTokens', attachTokens)
-    //  .run();
-//
-// function attachTokens($window) {
-//
-//   let attach = {
-//     request: (object) => {
-//
-//       let jwt = $window.localStorage.getItem('com.pillMeNow');
-//
-//       if (jwt) {
-//         object.headers['x-access-token'] = jwt;
-//       }
-//
-//       object.headers['Allow-Control-Allow-Origin'] = '*';
-//
-//       return object;
-//
-//     }
-//   };
-//
-//   return attach;
-//
-// }
-//
-// function appRun($rootScope, $state, Auth) {
-//   $rootScope.on('$stateChangeStart', (evt, toState) => {
-//
-//     if (toState.name === 'signin') return;
-//
-//     if (!Auth.isAuth() && toState.name !== 'signup') {
-//       evt.preventDefault();
-//       $state.go('signin');
-//     }
-//   });
-// }
+  });
